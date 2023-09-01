880b78a5fdf2b48e979f1135788937f1cfcc4253
KAPIL KUMAR BANSHEEWAL <88656401+kapilkumar12@users.noreply.github.com>
2023-09-01 23:14:50 +0530

    contact form start

---
+++ contact.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style-main.css">
+    <link rel="stylesheet" href="style-main.css">
+</head>
+
+
+<body>
+    <div class="container-fluid bg-rules contact-bg contact-overlay">
+        <div class="container">
+            <div class="row">
+                <h2 class="main-contact">Contact</h2>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row m-3">
+            <div class="col-md-4">
+                <div class="contact-paragraph text-center">
+                    <img src="images/map.png" alt="contact">
+                    <h3>Address Information</h3>
+                    <p>11009 new delhi</p>
+                </div>
+
+            </div>
+
+
+            <div class="col-md-4">
+                <div class="contact-paragraph text-center">
+                    <img src="images/smartphone.png" alt="contact">
+                    <h3>Contact Details</h3>
+                    <p>+91 9876543210</p>
+                    <p>digitalmarketing@gmail.com</p>
+                </div>
+
+            </div>
+
+            <div class="col-md-4">
+                <div class="text-center contact-paragraph">
+                    <img src="images/share.png" alt="contact">
+                    <h3>Stay In Touch</h3>
+                    <ul class="contact-ul">
+                        <a class="contact-icon" href="" target="_blink">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+                        <a class="contact-icon" href="" target="_blink"><i class="fa-brands fa-instagram"></i></a>
+                        <a class="contact-icon" href="" target="_blink"><i class="fa-brands fa-square-twitter"></i></a>
+                        <a class="contact-icon" href="" target="_blink"><i class="fa-brands fa-linkedin"></i></a>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="row text-center">
+            <h2>Contact Us</h2>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when
+                looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal</p>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
