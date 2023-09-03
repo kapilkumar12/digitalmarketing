313ddb47c92bdb21aae162f7700b4cf156b912a9
KAPIL KUMAR BANSHEEWAL <88656401+kapilkumar12@users.noreply.github.com>
2023-09-03 11:30:16 +0530

    contact form design

--- contact.php
+++ contact.php
@@ -68,21 +68,89 @@
             </div>
 
         </div>
+
         <div class="row text-center">
             <h2>Contact Us</h2>
             <p>It is a long established fact that a reader will be distracted by the readable content of a page when
                 looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal</p>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-md-6">
+    <div class="container-fluid contact-form-bg">
+        <div class="container">
+
+
+            <div class="row">
+                <div class="col-md-6">
+                    <form action="">
+
+
+
+                        <div class="mb-3">
+                            <label for="name" class="form-label contact-form-label">Name<span>*</span></label>
+                            <input type="text" class="form-control" id="name" aria-describedby="name" name="name"
+                                value="name" required>
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label contact-form-label">Email<span>*</span></label>
+                            <input type="email" class="form-control" id="email" aria-describedby="email" name="email"
+                                value="email" required>
+
+                        </div>
+
+
+                        <div class="mb-3">
+                            <label for="mobile" class="form-label contact-form-label">Mobile No.<span>*</span></label>
+                            <input type="number" class="form-control" id="mobile" aria-describedby="mobile"
+                                name="mobile" value="mobile" required>
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="select" class="form-label contact-form-label">What are you interested
+                                in?<span>*</span></label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+
+
+                        <div class="mb-3">
+                            <label for="message" class="form-label contact-form-label">Message<span>*</span></label>
+                            <textarea type="text" class="form-control" id="message" aria-describedby="message"
+                                name="message" value="message" rows="5" placeholder="message"></textarea>
+
+
+                            <div class="contact-btan">
+                                <button class="button-62" role="button">Submit</button>
+                            </div>
+
+                        </div>
+
+                    </form>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="contact-image">
+                        <img src="images/contact1.avif" alt="contact us">
+
+                    </div>
+                </div>
 
             </div>
-
         </div>
-
-
     </div>
+
+    <div>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d111994.33950524937!2d77.152256!3d28.694937600000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1693719932696!5m2!1sen!2sin"
+            width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
 
 </body>
 
